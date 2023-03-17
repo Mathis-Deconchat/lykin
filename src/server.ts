b1675f960c9c6bb738c169d0a4853ab1ccca91ec
Timothee Techer <timotheetecher.dev@gmail.com>
2023-03-17 12:18:45 +0100

    update

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,9 @@ import SubscriptionsLds from "@graphile/subscriptions-lds";
 
 dotenv.config();
 
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://user:pass@host:5432/dbname";
+const cors = require("cors");
+
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://user:pass@host:5432/dbname";
 const ROOT_DATABASE_URL = process.env.ROOT_DATABASE_URL || DATABASE_URL;
 const SCHEMA_NAMES = process.env.SCHEMA_NAMES
   ? process.env.SCHEMA_NAMES.split(",")
@@ -18,20 +19,21 @@ const SCHEMA_NAMES = process.env.SCHEMA_NAMES
 const PORT = process.env.PORT || 4000;
 
 const postgraphileOptions = {
-    watchPg: true,
-    graphiql: true,
-    enhanceGraphiql: true,
-    subscriptions: false,
-    appendPlugins: [
-        PgSimplifyInflectorPlugin,
-        PgManyToManyPlugin,
-        ConnectionFilterPlugin,
-        SubscriptionsLds,
-    ],
-}
+  watchPg: true,
+  graphiql: true,
+  enhanceGraphiql: true,
+  subscriptions: false,
+  appendPlugins: [
+    PgSimplifyInflectorPlugin,
+    PgManyToManyPlugin,
+    ConnectionFilterPlugin,
+    SubscriptionsLds,
+  ],
+};
 
 const app = express();
+app.use(cors());
 app.use(postgraphile(ROOT_DATABASE_URL, SCHEMA_NAMES, postgraphileOptions));
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
